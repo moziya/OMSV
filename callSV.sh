e7be4b0aaae9bafe396e2fc0a7c988cf01f9975b
Le LI <lile.moziya@gmail.com>
2017-09-07 12:12:14 +0800

    Update callSV.sh

--- callSV.sh
+++ callSV.sh
@@ -9,12 +9,12 @@ then
 
 echo "1. Start to call large indels"
 mkdir temp
-./OMSV -inputLabel 12878 -outputFolder ./ -SVoutputFile Indel -chrMapFile hg38_r.cmap -optAlignFile data/NA12878_700bp_hg38_combRefOMB2.oma -optTempFolder temp/
+./OMSV -inputLabel 12878 -outputFolder SV_result/ -SVoutputFile Indel -chrMapFile hg38_r.cmap -optAlignFile data/NA12878_700bp_hg38_combRefOMB2.oma -optTempFolder temp/
 rm -f *_List.txt
 ./postFilter.sh SV_result/12878Indel_2000.osv 2000
 
 echo "2. Start to call mixed indels"
-./OMSV_mixedIndel -inputLabel 12878 -outputFolder ./ -SVoutputFile Mixed_indel -chrMapFile hg38_r.cmap -optAlignFile data/NA12878_700bp_hg38_combRefOMB2.oma -optTempFolder temp/
+./OMSV_mixedIndel -inputLabel 12878 -outputFolder SV_result/ -SVoutputFile Mixed_indel -chrMapFile hg38_r.cmap -optAlignFile data/NA12878_700bp_hg38_combRefOMB2.oma -optTempFolder temp/
 ./postFilter.sh SV_result/12878Mixed_indel_2000.osv 2000
 sed '/Homozygous/d' SV_result/12878Mixed_indel_2000.osv | sed '/Heterozygous/d' > SV_result/12878Mixed_indel_2000.osv_tp && mv SV_result/12878Mixed_indel_2000.osv_tp SV_result/12878Mixed_indel_2000.osv
 
@@ -30,7 +30,7 @@ echo "3. Start to call large complex SVs"
 ./complex_caller.sh data/NA12878_700bp_hg38_OMB_split.oma . 12878
 
 echo "4. Start to call CNVs"
-echo "./CNV_caller.sh data/NA12878_700bp_hg38_combRefOMB2.oma hg38_r.cmap . 12878"
+./CNV_caller.sh data/NA12878_700bp_hg38_combRefOMB2.oma hg38_r.cmap . 12878
 
 echo "5. Compile complex SV list"
 ./compile_complex.sh 12878Med_inv.osv 12878Duplication_2.bed 12878Complex.bed SV_result/12878Complex_total.bed
