d3b80746c1f0f5d09973c3bd700b8d32f6a23784
Le LI <lile.moziya@gmail.com>
2017-09-07 12:34:39 +0800

    Update callSV.sh

--- callSV.sh
+++ callSV.sh
@@ -22,15 +22,24 @@ sed '/Homozygous/d' SV_result/12878Mixed_indel_2000.osv | sed '/Heterozygous/d' 
 rm -r -f temp
 fi
 
-
-echo "2. Start to call medium-size inversions"
-java -jar OMTools/OMTools.jar SVDetection --refmapin hg38_r.cmap --optresin data/NA12878_700bp_hg38_combRefOMB2.oma --mininvsig 4 --svout 12878Med_inv.osv --flanksig 0 --deg 0 -svmode 2 -minsupport 10 && sed -i '/.site/d' 12878Med_inv.osv
+if java -jar OMTools-master/OMTools.jar >/dev/null 2>&1
+then
+        echo "2. Start to call medium-size inversions"
+	java -jar OMTools/OMTools.jar SVDetection --refmapin hg38_r.cmap --optresin data/NA12878_700bp_hg38_combRefOMB2.oma --mininvsig 4 --svout 12878Med_inv.osv --flanksig 0 --deg 0 -svmode 2 -minsupport 10 && sed -i '/.site/d' 12878Med_inv.osv
+else
+        echo "Medium-size inversion caller requires java and OMTools but not installed. Skip this step."
+fi
 
 echo "3. Start to call large complex SVs"
 ./complex_caller.sh data/NA12878_700bp_hg38_OMB_split.oma . 12878
 
-echo "4. Start to call CNVs"
-./CNV_caller.sh data/NA12878_700bp_hg38_combRefOMB2.oma hg38_r.cmap . 12878
+if command -v matlab >/dev/null 2>&1
+then
+	echo "4. Start to call CNVs"
+	./CNV_caller.sh data/NA12878_700bp_hg38_combRefOMB2.oma hg38_r.cmap . 12878
+else
+	echo "CNV caller requires 'matlab' but not installed. Skip this step."
+fi
 
 echo "5. Compile complex SV list"
 ./compile_complex.sh 12878Med_inv.osv 12878Duplication_2.bed 12878Complex.bed SV_result/12878Complex_total.bed
